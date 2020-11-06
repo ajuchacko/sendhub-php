b24f18d5e2155e8a91674283f72d19f7082ec808
aju <ajuchacko91@gmail.com>
2020-11-06 23:40:24 +0530

    gets all contacts

---
+++ Sendhub.php
@@ -0,0 +1,46 @@
+<?php
+
+class SendHub {
+
+	private $api_key;
+	private $number;
+
+	public function __construct(string $api_key, string $number)
+	{
+		$this->api_key = $api_key;
+		$this->number = $number;
+	}
+
+	public function contacts(array $options = [], $contact_id = false)
+	{
+		if (count($options) > 0 && empty($options['contact_id'])) {
+			$api_url = $this->baseUrl() . "contacts/" . $this->credentials() + '&' + http_build_query($options);
+		} elseif (count($options) > 0 && !empty($options['contact_id'])) {
+			$api_url = $this->baseUrl() . 'contacts/' . $options['contact_id'] . $this->credentials() . '&' . http_build_query($options);
+		} else {
+			$api_url = $this->baseUrl() . 'contacts' + $this->credentials();
+		}
+
+		return $this->sendRequest('get', $api_url, ['body' => []]);
+	}
+
+	private function baseUrl()
+	{
+		return "https://api.sendhub.com/v1/";
+	}
+
+	private function credentials()
+	{
+		return "/?username=#{number}&api_key=#{api_key}";
+	}
+
+	private function apiKey()
+	{
+		return $this->api_key;
+	}
+
+	private function number()
+	{
+		return $this->number;
+	}
+}
