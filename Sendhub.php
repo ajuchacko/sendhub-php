a513957d105a8525dbb0a03277a3ae206dfa6262
aju <ajuchacko91@gmail.com>
2020-11-10 00:08:59 +0530

    adds overloading method __call

--- Sendhub.php
+++ Sendhub.php
@@ -11,6 +11,28 @@ class SendHub {
 		$this->number = $number;
 	}
 
+	public function __call($method, $options)
+	{
+		$method = explode('_', $method);
+
+		if ($method[0] == 'put' || method[0] == 'delete') {
+			if (($last = $method[count($method) - 1]) == 'messages') {
+				$api_url = $this->baseUrl() . $last . '/' . $options['id'] . $this->credentials();
+				unset($options['id']);
+			} else {
+				$api_url = $this->baseUrl() . $method[count($method) - 1] . '/' . $options['id'] . $this->credentials();
+			}
+		} elseif ($method[0] == 'get' && ($method[count($method) - 1] == 'messages' || !is_null($options['id']))) {
+			$api_url = $this->baseUrl() . $method[count($method) - 1] . '/' . $options['id'] . $this->credentials();
+		} else {
+			$api_url = $this->baseUrl() . $method[count($method) - 1] . $this->credentials();
+		}
+
+		$request = $this->sendRequest($method[0], $api_url, ['body' => json_encode($options)]);
+
+		is_null($request) && $method[0] == 'delete' ? "Aaaand it\'s gone" : request;
+	}
+
 	public function contacts(array $options = [], $contact_id = false)
 	{
 		if (count($options) > 0 && empty($options['contact_id'])) {
